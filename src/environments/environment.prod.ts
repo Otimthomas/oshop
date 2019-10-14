417539d5ba62825bf7acda055832bc4c23350977
otimthomas <totim444@hotmail.co.uk>
2019-10-15 02:19:34 +0300

    Add routing and create a navbar component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA6T0zcI3PaL88lTbafwaLo-6aw86Ra--k",
-    authDomain: "oshop-4972a.firebaseapp.com",
-    databaseURL: "https://oshop-4972a.firebaseio.com",
-    projectId: "oshop-4972a",
-    storageBucket: "oshop-4972a.appspot.com",
-    messagingSenderId: "445196904833",
-    appId: "1:445196904833:web:73dfd76f6fc674ccc36080",
-    measurementId: "G-7P456WG3NW"
+    apiKey: "AIzaSyDisuxjuK8j-rQ9rznsAT2esAwKOI7hDRQ",
+    authDomain: "oshop-d9b78.firebaseapp.com",
+    databaseURL: "https://oshop-d9b78.firebaseio.com",
+    projectId: "oshop-d9b78",
+    storageBucket: "oshop-d9b78.appspot.com",
+    messagingSenderId: "396539096988",
+    appId: "1:396539096988:web:f93dc065ab9d3de273e259",
+    measurementId: "G-2LB0QT2P1P"
   }
 };
